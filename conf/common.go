6f5093627b21ecb7d350bcc2d04ea08958de3d0e
sophia <askitachi@163.com>
2022-12-15 11:38:21 +0800

    first commit

--- conf/common.go
+++
@@ -1,9 +0,0 @@
-package conf
-
-const (
-	CookieDomain = "43.138.211.58"
-	CookieTime   = 3600
-	ImageUrl     = "../static/img/avatar.png" //所有用户头像都是使用的这个地址...还待fix
-
-	CephPath = "/home/sophia/data/ceph/"
-)
